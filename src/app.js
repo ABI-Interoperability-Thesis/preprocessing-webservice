70c994e48cbd6bcc8fceea0016c06e9615787b59
JoaoGuedes <joaoguedes.cjp@gmail.com>
2023-10-18 09:36:50 +0100

    fix: fixed publishing
    mysql-webservice

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // Landing Page for the webservice
-app.get('/', (req,res)=>res.send('Landing Route for Preprocessing Webservice test'))
+app.get('/', (req,res)=>res.send('Landing Route for Preprocessing Webservice test #version something'))
 
 //Routing /api requests to the api router
 const apiRoutes = require('./routes/api-routes')
